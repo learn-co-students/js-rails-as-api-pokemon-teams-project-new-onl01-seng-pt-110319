efbbb7711c7212b6275b8ce9ba5ded1da099799b
nicksolony <nicksolony@gmail.com>
2020-07-12 17:13:15 -0400

    building elements

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,31 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  fetch(TRAINERS_URL)
+  .then(response=>response.json())
+  .then(data=>{
+    data.data.forEach((trainer) =>  console.log(trainer.attributes.name));
+    console.log(data,data.data)
+    });
+});
+
+function createTrainerCard(trainer) {
+  const card = document.createElement('div')
+  card.className='card'
+  card.setAttribute('data-id',trainer.id)
+  const name=document.createElement('p')
+  name.innerText=trainer.name
+  const addButton = document.createElement('button')
+  addButton.setAttribute('data-trainer-id',trainer.id)
+  addButton.innerText='Add Pokemon'
+
+  card.appendChild(name)
+  card.appendChild(addButton)
+  const main = document.querySelector('main')
+  main.appendChild(card)
+
+
+}
