c31e72b92b7f9ed2e4bad920fa5356a413c1145f
nicksolony <nicksolony@gmail.com>
2020-07-12 17:57:58 -0400

    fetched all date into html

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -7,8 +7,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
   fetch(TRAINERS_URL)
   .then(response=>response.json())
   .then(data=>{
-    data.data.forEach((trainer) =>  console.log(trainer.attributes.name));
-    console.log(data,data.data)
+    data.data.forEach((trainer) => createTrainerCard(trainer.attributes));
+    data.included.forEach((pokemon) => {
+      addPokemon(pokemon.attributes);
+    });
+    console.log(data)
     });
 });
 
@@ -21,11 +24,24 @@ function createTrainerCard(trainer) {
   const addButton = document.createElement('button')
   addButton.setAttribute('data-trainer-id',trainer.id)
   addButton.innerText='Add Pokemon'
+  const team=document.createElement('ul')
 
   card.appendChild(name)
   card.appendChild(addButton)
+  card.appendChild(team)
   const main = document.querySelector('main')
   main.appendChild(card)
+}
 
-
+function addPokemon(pokemon) {
+  const teamMember=document.createElement('li')
+  teamMember.innerText = `${pokemon.nickname} (${pokemon.species})`
+  const releaseButton = document.createElement('button')
+  releaseButton.innerText='Release'
+  releaseButton.className='release'
+  releaseButton.setAttribute('data-pokemon-id',pokemon.id)
+  teamMember.appendChild(releaseButton)
+  const trainerCard=document.querySelector(`[data-id='${pokemon.trainer_id}']`);
+  const team=trainerCard.querySelector('ul')
+  team.appendChild(teamMember)
 }
