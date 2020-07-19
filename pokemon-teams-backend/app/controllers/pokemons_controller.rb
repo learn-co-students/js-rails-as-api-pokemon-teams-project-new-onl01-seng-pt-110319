51e22728ba03cf87e21af269945631b30e838742
github username <github email address>
2020-07-18 20:40:10 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,19 @@
+class PokemonsController < ApplicationController
+    def create
+        
+        pokemon = Pokemon.new(nickname: Faker::Name.first_name, species: Faker::Games::Pokemon.name, trainer_id: params[:trainer_id])
+        
+        
+        if pokemon
+            render json: PokemonSerializer.new(pokemon)
+        else
+            render json: {message: "Pokemon could not be created"}
+        end
+    end
+
+    def destroy
+        pokemon = Pokemon.find(params[:id])
+        pokemon.delete
+    end
+end
+ 
